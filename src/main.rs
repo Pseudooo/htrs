db7ddd5e3dda646fafc8006609c4f414e945916f
Mitchell Wright <mitchell.wright877@gmail.com>
2025-06-19 21:33:15 +0100

    Scaffold basic subcommands

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: CliCommands
+}
+
+#[derive(Subcommand)]
+enum CliCommands {
+    #[command(alias = "c")]
+    Call,
+    #[command(alias = "cfg")]
+    Config,
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    match Cli::try_parse() {
+        Ok(x) => {
+            exec(x);
+        }
+        Err(e) => {
+            e.exit();
+        }
+    }
+}
+
+fn exec(arg: Cli) {
+    match arg.command {
+        CliCommands::Call => {
+            println!("Using Call")
+        },
+        CliCommands::Config => {
+            println!("Using config");
+        }
+    }
 }
