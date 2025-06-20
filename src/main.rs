a40456fef3f8e2d5bbd6dd66075effe01dc996b8
Mitchell Wright <mitchell.wright877@gmail.com>
2025-06-20 12:50:28 +0100

    Split config into own module

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,16 @@
 mod command_args;
+mod htrs_config;
 
 use crate::command_args::RootCommands::{Call, Service};
 use crate::command_args::ServiceCommands::{Add, Remove};
 use crate::command_args::{CallOpts, Cli, ServiceCommands};
+use crate::htrs_config::{HtrsConfig, ServiceConfig};
 use clap::Parser;
 use serde::{Deserialize, Serialize};
-use std::fs::{File, OpenOptions};
-use std::path::Path;
-
-#[derive(Serialize, Deserialize)]
-struct HtrsConfig {
-    services: Vec<ServiceConfig>,
-}
-
-impl HtrsConfig {
-    fn new() -> HtrsConfig {
-        HtrsConfig { services: Vec::new() }
-    }
-
-    fn service_defined(&self, name: &str) -> bool {
-        for service in &self.services {
-            if service.name == name {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    fn find_service_config(&self, name: &str) -> Option<&ServiceConfig> {
-        for service in &self.services {
-            if service.name == name {
-                return Some(service);
-            }
-        }
-        None
-    }
-}
-
-#[derive(Serialize, Deserialize)]
-struct ServiceConfig {
-    name: String,
-    host: String,
-}
-
-impl ServiceConfig {
-    fn new(name: String, host: String) -> ServiceConfig {
-        ServiceConfig { name, host }
-    }
-}
 
 fn main() {
     let parsed_args = Cli::parse();
-    let mut config = ensure_config();
+    let mut config = HtrsConfig::load("./htrs_config.json");
 
     match parsed_args.command {
         Service(service_command) => {
@@ -73,15 +32,14 @@ fn execute_service_command(config: &mut HtrsConfig, cmd: &ServiceCommands) {
             }
 
             config.services.push(ServiceConfig::new(name.clone(), host.clone()));
-
-            save_config(config);
+            config.save("./htrs_config.json");
         },
 
         Remove { name } => {
             if config.service_defined(name) {
                 config.services.retain(|x| !x.name.eq(name));
                 println!("Service {} removed", name);
-                save_config(config);
+                config.save("./htrs_config.json");
             } else {
                 panic!("Service with name {} not found", name)
             }
@@ -114,34 +72,4 @@ fn execute_call_command(config: &HtrsConfig, cmd: CallOpts) {
     } else {
         panic!("Service not found");
     }
-}
-
-fn ensure_config() -> HtrsConfig {
-    let config_path = Path::new("./htrs_config.json");
-    if config_path.exists() {
-        let file = File::open(config_path)
-            .expect("Unable to read htrs_config.json");
-        let config: HtrsConfig = serde_json::from_reader(file)
-            .expect("Unable to read htrs_config.json");
-        return config;
-    }
-
-    let mut file = File::create(config_path)
-        .expect("Unable to create htrs_config.json");
-
-    let blank_config = HtrsConfig::new();
-    serde_json::to_writer_pretty(&mut file, &blank_config)
-        .expect("Unable to write config to htrs_config.json");
-    return blank_config;
-}
-
-fn save_config(config: &mut HtrsConfig) {
-    let config_path = Path::new("./htrs_config.json");
-    let mut file = OpenOptions::new()
-        .write(true)
-        .truncate(true)
-        .open(config_path)
-        .expect("Unable to write updated config to htrs_config.json");
-    serde_json::to_writer_pretty(&mut file, &config)
-        .expect("Unable to write updated config to htrs_config.json");
 }
