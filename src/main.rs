a048352d0ec1e7e1feda2916671a1dab42ae7a66
Mitchell Wright <mitchell.wright877@gmail.com>
2025-06-21 00:32:07 +0100

    Tightened up all happy paths

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use crate::command_args::ServiceCommands::{Add, Environment, Remove};
 use crate::command_args::{CallOpts, Cli, EnvironmentCommands, ServiceCommands};
 use crate::htrs_config::{HtrsConfig, ServiceConfig, ServiceEnvironmentConfig};
 use clap::Parser;
+use reqwest::blocking::Response;
 use std::error::Error;
 use std::fmt;
 
@@ -32,6 +33,18 @@ impl Error for HtrsError {
     }
 }
 
+struct HtrsOutcome<'a> {
+    config: &'a HtrsConfig,
+    config_updated: bool,
+    outcome_dialogue: String,
+}
+
+impl<'a> HtrsOutcome<'a> {
+    fn new(config: &'a HtrsConfig, config_updated: bool, outcome_dialogue: String) -> HtrsOutcome<'a> {
+        HtrsOutcome { config, config_updated, outcome_dialogue}
+    }
+}
+
 fn main() {
     let parsed_args = Cli::parse();
     let mut config = HtrsConfig::load("./htrs_config.json");
@@ -45,12 +58,23 @@ fn main() {
         },
     };
 
-    if let Err(e) = result {
-        println!("{}", e.details);
+    match result {
+        Err(e) => {
+            println!("{}", e.details);
+            return;
+        },
+        Ok(outcome) => {
+            let dialogue = outcome.outcome_dialogue;
+            if outcome.config_updated {
+                outcome.config.save("htrs_config.json");
+            }
+
+            println!("{}", dialogue);
+        }
     }
 }
 
-fn execute_service_command(config: &mut HtrsConfig, cmd: &ServiceCommands) -> Result<(), HtrsError> {
+fn execute_service_command<'a>(config: &'a mut HtrsConfig, cmd: &ServiceCommands) -> Result<HtrsOutcome<'a>, HtrsError> {
     match cmd {
         Add { name } => {
             for service in config.services.iter() {
@@ -60,9 +84,11 @@ fn execute_service_command(config: &mut HtrsConfig, cmd: &ServiceCommands) -> Re
             }
 
             config.services.push(ServiceConfig::new(name.clone()));
-            config.save("./htrs_config.json");
-            println!("Service \"{name}\" created");
-            Ok(())
+            Ok(HtrsOutcome::new(
+                config,
+                true,
+                format!("Service \"{name}\" created")
+            ))
         },
 
         Remove { name } => {
@@ -70,21 +96,29 @@ fn execute_service_command(config: &mut HtrsConfig, cmd: &ServiceCommands) -> Re
                 config.services.retain(|x| !x.name.eq(name));
                 println!("Service {} removed", name);
                 config.save("./htrs_config.json");
-                Ok(())
+                Ok(HtrsOutcome::new(
+                    config,
+                    true,
+                    format!("Service \"{name}\" removed"),
+                ))
             } else {
                 Err(HtrsError::new(&format!("Service \"{name}\" does not exist")))
             }
         }
 
-        ServiceCommands::List => {
-            if config.services.len() == 0 {
-                println!("No services defined");
-            } else {
-                for service in &config.services {
-                    println!(" - {}", service.name);
-                }
-            }
-            Ok(())
+        ServiceCommands::List => match config.services.len() {
+            0 => Ok(HtrsOutcome::new(
+                config,
+                false,
+                "No services found".to_string(),
+            )),
+            _ => Ok(HtrsOutcome::new(
+                config,
+                false,
+                format!(" - {}", config.services.iter().map(|service| service.name.clone())
+                    .collect::<Vec<String>>()
+                    .join("\n - ")),
+            ))
         },
 
         Environment(env_command) => {
@@ -93,7 +127,7 @@ fn execute_service_command(config: &mut HtrsConfig, cmd: &ServiceCommands) -> Re
     }
 }
 
-fn execute_environment_command(config: &mut HtrsConfig, cmd: &EnvironmentCommands) -> Result<(), HtrsError> {
+fn execute_environment_command<'a>(config: &'a mut HtrsConfig, cmd: &EnvironmentCommands) -> Result<HtrsOutcome<'a>, HtrsError> {
     match cmd {
         EnvironmentCommands::Add { service_name, name: environment_name, host, default } => {
             if let Some(service) = config.find_service_config_mut(&service_name) {
@@ -107,9 +141,11 @@ fn execute_environment_command(config: &mut HtrsConfig, cmd: &EnvironmentCommand
                     }
 
                     service.environments.push(ServiceEnvironmentConfig::new(environment_name.clone(), host.clone(), default.clone()));
-                    config.save("./htrs_config.json");
-                    println!("Environment {environment_name} created for {service_name}");
-                    Ok(())
+                    Ok(HtrsOutcome::new(
+                        config,
+                        true,
+                        format!("Environment \"{environment_name}\" created for {service_name}"),
+                    ))
                 }
             } else {
                 Err(HtrsError::new(&format!("Service {service_name} does not exist")))
@@ -121,14 +157,19 @@ fn execute_environment_command(config: &mut HtrsConfig, cmd: &EnvironmentCommand
                 if service.environments.len() == 0 {
                     Err(HtrsError::new(&format!("No environments defined for {service_name}")))
                 } else {
-                    for environment in &service.environments {
-                        if environment.default {
-                            println!("{}: {} (default)", environment.name, environment.host);
-                        } else {
-                            println!("{}: {}", environment.name, environment.host);
-                        }
-                    }
-                    Ok(())
+                    let environment_list = service.environments.iter()
+                        .map(|env| match env.default {
+                            true => format!(" - {}: {} (default)", env.name, env.host),
+                            false => format!(" - {}: {}", env.name, env.host),
+                        })
+                        .collect::<Vec<String>>()
+                        .join("\n");
+
+                    Ok(HtrsOutcome::new(
+                        config,
+                        false,
+                        environment_list,
+                    ))
                 }
             } else {
                 Err(HtrsError::new(&format!("Service {service_name} does not exist")))
@@ -138,8 +179,11 @@ fn execute_environment_command(config: &mut HtrsConfig, cmd: &EnvironmentCommand
         EnvironmentCommands::Remove { service_name, environment_name } => {
             if let Some(service) = config.find_service_config_mut(&service_name) {
                 service.remove_environment(environment_name);
-                println!("Environment {environment_name} removed for {service_name}");
-                Ok(())
+                Ok(HtrsOutcome::new(
+                    config,
+                    true,
+                    format!("Environment {environment_name} removed for {service_name}"),
+                ))
             } else {
                 Err(HtrsError::new(&format!("Service {service_name} does not exist")))
             }
@@ -147,18 +191,32 @@ fn execute_environment_command(config: &mut HtrsConfig, cmd: &EnvironmentCommand
     }
 }
 
-fn execute_call_command(config: &HtrsConfig, cmd: CallOpts) -> Result<(), HtrsError> {
+fn execute_call_command(config: &HtrsConfig, cmd: CallOpts) -> Result<HtrsOutcome, HtrsError> {
     if let Some(service) = config.find_service_config(&cmd.service_name) {
         if let Some(environment_name) = cmd.environment {
             if let Some(environment) = service.find_environment(&environment_name) {
                 let uri = format!("https://{}/", environment.host);
-                make_get_request(&uri)
+                match make_get_request(&uri) {
+                    Ok(response) => Ok(HtrsOutcome::new(
+                        config,
+                        false,
+                        format!("Received {} response", response.status()),
+                    )),
+                    Err(e) => Err(e),
+                }
             } else {
                 Err(HtrsError::new(&format!("No environments defined for {}", service.name)))
             }
         } else if let Some(default_environment) = service.find_default_environment() {
             let uri = format!("https://{}/", default_environment.host);
-            make_get_request(&uri)
+            match make_get_request(&uri) {
+                Ok(response) => Ok(HtrsOutcome::new(
+                    config,
+                    false,
+                    format!("Received {} response", response.status()),
+                )),
+                Err(e) => Err(e),
+            }
         } else {
             Err(HtrsError::new(&format!("No default environment defined for {}", cmd.service_name)))
         }
@@ -167,13 +225,10 @@ fn execute_call_command(config: &HtrsConfig, cmd: CallOpts) -> Result<(), HtrsEr
     }
 }
 
-fn make_get_request(url: &str) -> Result<(), HtrsError> {
+fn make_get_request(url: &str) -> Result<Response, HtrsError> {
     let client = reqwest::blocking::Client::new();
     match client.get(url).send() {
-        Ok(response) => {
-            println!("Receieved {} response", response.status());
-            Ok(())
-        },
+        Ok(response) => Ok(response),
         Err(e) => {
             Err(HtrsError::new(&format!("Failed to call {} response: {}", url, e)))
         }
