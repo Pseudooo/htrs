01e6b61a015b3163d909ecd7b20d240a4e25584a
Mitchell Wright <mitchell.wright877@gmail.com>
2025-06-25 21:01:15 +0100

    Handling outcome in main

--- src/main.rs
+++ src/main.rs
@@ -7,8 +7,12 @@ use crate::command_args::Cli;
 use crate::command_args::RootCommands::GenerateMarkdown;
 use crate::commands::execute_command;
 use crate::htrs_config::VersionedHtrsConfig;
+use crate::outcomes::{HtrsAction, HtrsError};
 use clap::Parser;
 use clap_markdown::print_help_markdown;
+use reqwest::blocking::{Client, Response};
+use reqwest::Method;
+use HtrsAction::MakeRequest;
 
 fn main() {
     let parsed_args = Cli::parse();
@@ -34,8 +38,33 @@ fn main() {
                 VersionedHtrsConfig::save(config, "./htrs_config.json")
             }
 
+            if let Some(action) = outcome.action {
+                match handle_outcome_action(action) {
+                    Ok(response) => println!("Received {} response", response.status()),
+                    Err(e) => println!("Failed to call: {e}"),
+                }
+            }
+
             println!("{}", dialogue);
         }
+    }
+}
+
+fn handle_outcome_action(action: HtrsAction) -> Result<Response, HtrsError> {
+    let MakeRequest { url, headers } = action;
+    let client = Client::new();
+    let mut request_builder = client.request(Method::GET, url);
+    for (key, value) in headers {
+        request_builder = request_builder.header(key.as_str(), value.as_str());
+    }
+
+    let request = match request_builder.build() {
+        Ok(req) => req,
+        Err(e) => return Err(HtrsError::new(&e.to_string())),
+    };
+    match client.execute(request) {
+        Ok(res) => Ok(res),
+        Err(e) => Err(HtrsError::new(&e.to_string())),
     }
 }
 
