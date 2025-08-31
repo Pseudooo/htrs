c42eaa92dbba340c63080c18d7b80768179e25fd
Mitchell Wright <mitchell.wright877@gmail.com>
2025-08-31 12:30:45 +0100

    Added test coverage for cmd execution

--- src/commands/environment_commands.rs
+++ src/commands/environment_commands.rs
@@ -164,10 +164,10 @@ fn remove_environment(config: &mut HtrsConfig, service_name: &str, environment_n
     let Some(service) = config.get_service_mut(service_name) else {
         return Err(HtrsError::new(format!("Service '{service_name}' not found").as_str()))
     };
-    if service.remove_environment(environment_name) {
-        return Err(HtrsError::new(format!("Service '{service_name}' has no environment '{environment_name}'").as_str()));
+    match service.remove_environment(environment_name) {
+        true => Ok(UpdateConfig),
+        false => Err(HtrsError::new(format!("Service '{service_name}' has no environment '{environment_name}'").as_str())),
     }
-    Ok(UpdateConfig)
 }
 
 fn list_environments(config: &HtrsConfig, service_name: &str) -> Result<HtrsAction, HtrsError> {
@@ -175,10 +175,14 @@ fn list_environments(config: &HtrsConfig, service_name: &str) -> Result<HtrsActi
         return Err(HtrsError::new(format!("Service '{service_name}' not found").as_str()));
     };
 
+    if service.environments.len() == 0 {
+        return Ok(PrintDialogue(format!("Service '{}' has no environments", service.name)))
+    }
+
     let dialogue = service.environments.iter()
         .map(|e| match &e.alias {
-            Some(alias) => format!("- {} ({})", e.name, alias),
-            None => format!("- {}", e.name),
+            Some(alias) => format!(" - {} ({})", e.name, alias),
+            None => format!(" - {}", e.name),
         })
         .collect::<Vec<String>>()
         .join("\n");
@@ -286,5 +290,192 @@ mod environment_command_binding_tests {
             panic!("Command is not EnvironmentCommand::List");
         };
         assert_eq!(service, "foo_service");
+    }
+}
+
+#[cfg(test)]
+mod environment_command_execution_tests {
+    use super::*;
+    use crate::test_helpers::{HtrsConfigBuilder, HtrsServiceBuilder};
+    use rstest::rstest;
+
+    #[rstest]
+    #[case(Some("foo_alias".to_string()))]
+    #[case(None)]
+    fn given_known_service_when_add_environment_then_should_add_environment(
+        #[case] alias: Option<String>,
+    ) {
+        let mut config = HtrsConfigBuilder::new()
+            .with_service(
+                HtrsServiceBuilder::new()
+                    .with_name("foo_service")
+            )
+            .build();
+        let command = EnvironmentCommand::Add {
+            service: "foo_service".to_string(),
+            name: "foo_environment".to_string(),
+            alias: alias.clone(),
+            host: "host.com".to_string(),
+            default: true,
+        };
+
+        let result = command.execute_command(&mut config);
+        assert!(result.is_ok(), "{}", result.err().unwrap().to_string());
+        assert!(matches!(result.unwrap(), UpdateConfig));
+
+        let new_environment = config.get_service("foo_service")
+            .unwrap()
+            .get_environment("foo_environment");
+        assert!(new_environment.is_some(), "{}", "Environment not found in config");
+        assert_eq!(new_environment.unwrap().name, "foo_environment");
+        assert_eq!(new_environment.unwrap().alias, alias);
+        assert_eq!(new_environment.unwrap().host, "host.com");
+        assert_eq!(new_environment.unwrap().default, true);
+    }
+
+    #[test]
+    fn given_unknown_service_when_add_environment_then_should_error() {
+        let mut config = HtrsConfigBuilder::new()
+            .build();
+        let command = EnvironmentCommand::Add {
+            service: "foo_service".to_string(),
+            name: "foo_environment".to_string(),
+            alias: None,
+            host: "host.com".to_string(),
+            default: true,
+        };
+
+        let result = command.execute_command(&mut config);
+
+        assert!(result.is_err(), "Returned result was not an error");
+    }
+
+    #[rstest]
+    #[case("existing_environment", "new_alias")]
+    #[case("new_environment", "existing_alias")]
+    fn given_known_service_when_add_existing_environment_then_should_error(
+        #[case] name: String,
+        #[case] alias: String,
+    ) {
+        let mut config = HtrsConfigBuilder::new()
+            .with_service(
+                HtrsServiceBuilder::new()
+                    .with_name("foo_service")
+                    .with_environment("existing_environment", Some("existing_alias"), "host.com", true)
+            )
+            .build();
+        let command = EnvironmentCommand::Add {
+            service: "foo_service".to_string(),
+            name: name.clone(),
+            alias: Some(alias),
+            host: "foo.com".to_string(),
+            default: false,
+        };
+
+        let result = command.execute_command(&mut config);
+
+        assert!(result.is_err(), "{}", "Did not return error");
+        let service = config.get_service("foo_service").unwrap();
+        assert_eq!(service.environments.len(), 1);
+    }
+
+    #[rstest]
+    #[case("foo_environment")]
+    #[case("foo_alias")]
+    fn given_known_service_and_environment_when_execute_remove_then_should_remove(
+        #[case] name: String,
+    ) {
+        let mut config = HtrsConfigBuilder::new()
+            .with_service(
+                HtrsServiceBuilder::new()
+                    .with_name("foo_service")
+                    .with_environment("foo_environment", Some("foo_alias"), "host.com", true)
+            )
+            .build();
+        let command = EnvironmentCommand::Remove {
+            service: "foo_service".to_string(),
+            environment: name,
+        };
+
+        let result = command.execute_command(&mut config);
+
+        assert!(result.is_ok(), "{}", result.err().unwrap().to_string());
+        assert!(matches!(result.unwrap(), UpdateConfig), "Result was not HtrsAction::UpdateConfig");
+        let service = config.get_service("foo_service").unwrap();
+        assert_eq!(service.environments.len(), 0);
+    }
+
+    #[test]
+    fn given_unknown_service_when_remove_environment_then_should_error() {
+        let mut config = HtrsConfigBuilder::new()
+            .build();
+        let command = EnvironmentCommand::Remove {
+            service: "foo_service".to_string(),
+            environment: "foo_environment".to_string(),
+        };
+
+        let result = command.execute_command(&mut config);
+
+        assert!(result.is_err(), "Did not return error");
+    }
+
+    #[test]
+    fn given_known_service_and_unknown_environment_when_remove_environment_then_should_error() {
+        let mut config = HtrsConfigBuilder::new()
+            .with_service(
+                HtrsServiceBuilder::new()
+                    .with_name("foo_service")
+            )
+            .build();
+        let command = EnvironmentCommand::Remove {
+            service: "foo_service".to_string(),
+            environment: "foo_environment".to_string(),
+        };
+
+        let result = command.execute_command(&mut config);
+
+        assert!(result.is_err(), "Returned result was not an error");
+    }
+
+    #[rstest]
+    #[case("foo_service")]
+    #[case("foo_service_alias")]
+    fn given_known_service_and_known_environments_when_list_then_should_list(
+        #[case] service_name: String,
+    ) {
+        let mut config = HtrsConfigBuilder::new()
+            .with_service(
+                HtrsServiceBuilder::new()
+                    .with_name("foo_service")
+                    .with_alias("foo_service_alias")
+                    .with_environment("environment1", Some("alias1"), "host1.com", true)
+                    .with_environment("environment2", None, "host2.com", true)
+            )
+            .build();
+        let command = EnvironmentCommand::List {
+            service: service_name,
+        };
+
+        let result = command.execute_command(&mut config);
+
+        assert!(result.is_ok(), "{}", result.err().unwrap().to_string());
+        let PrintDialogue(dialogue) = result.unwrap() else {
+            panic!("Returned action was not HtrsAction::PrintDialogue");
+        };
+        assert!(dialogue.contains(" - environment1 (alias1)"));
+        assert!(dialogue.contains(" - environment2"));
+    }
+
+    #[test]
+    fn given_unknown_service_when_list_then_should_error() {
+        let mut config = HtrsConfigBuilder::new()
+            .build();
+        let command = EnvironmentCommand::List {
+            service: "foo_service".to_string(),
+        };
+
+        let result = command.execute_command(&mut config);
+
+        assert!(result.is_err(), "Did not return error");
     }
 }
