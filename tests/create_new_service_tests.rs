eef9008528c2b7677118d5abaf6c3d03740c5c01
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-17 21:41:24 +0100

    Added service int tests

--- tests/create_new_service_tests.rs
+++ tests/create_new_service_tests.rs
@@ -1,43 +1,85 @@
-mod helpers;
+mod common;
 
-use assert_cmd::prelude::*;
-use crate::helpers::{get_config, setup};
-// Add methods on commands
-use predicates::prelude::*;
-use std::error::Error;
-use std::process::Command;
+#[cfg(test)]
+mod create_new_service_tests {
+    use crate::common::{get_config, setup, HtrsConfigBuilder, ServiceBuilder};
+    use assert_cmd::prelude::{CommandCargoExt, OutputAssertExt};
+    use std::error::Error;
+    use std::process::Command;
 
-#[test]
-fn given_create_new_service_when_no_arguments_then_should_err() -> Result<(), Box<dyn Error>> {
-    setup();
+    #[test]
+    fn given_create_new_service_when_no_arguments_then_should_err() -> Result<(), Box<dyn Error>> {
+        setup(None);
 
-    let mut cmd = Command::cargo_bin("htrs")?;
+        let mut cmd = Command::cargo_bin("htrs")?;
 
-    cmd.arg("new")
-        .arg("service");
-    cmd.assert()
-        .failure();
+        cmd.arg("new")
+            .arg("service");
+        cmd.assert()
+            .failure();
 
-    Ok(())
-}
+        Ok(())
+    }
 
-#[test]
-fn given_valid_create_new_service_command_then_should_succeed() -> Result<(), Box<dyn Error>> {
-    setup();
+    #[test]
+    pub fn given_valid_create_new_service_command_without_alias_then_should_succeed() -> Result<(), Box<dyn Error>> {
+        setup(None);
 
-    let mut cmd = Command::cargo_bin("htrs")?;
+        let mut cmd = Command::cargo_bin("htrs")?;
 
-    cmd.arg("new")
-        .arg("service")
-        .arg("foo")
-        .arg("--alias").arg("bar");
-    cmd.assert()
-        .success();
+        cmd.arg("new")
+            .arg("service")
+            .arg("foo");
+        cmd.assert()
+            .success();
 
-    let config = get_config();
-    let service = &config["services"][0];
-    assert_eq!(service["name"], "foo");
-    assert_eq!(service["alias"], "bar");
+        let config = get_config();
+        let service = config.get_service("foo").unwrap();
+        assert_eq!(service.name, "foo");
+        assert_eq!(service.alias, None);
 
-    Ok(())
+        Ok(())
+    }
+
+    #[test]
+    fn given_valid_create_new_service_command_then_should_succeed() -> Result<(), Box<dyn Error>> {
+        setup(None);
+
+        let mut cmd = Command::cargo_bin("htrs")?;
+
+        cmd.arg("new")
+            .arg("service")
+            .arg("foo")
+            .arg("--alias").arg("bar");
+        cmd.assert()
+            .success();
+
+        let config = get_config();
+        let service = config.get_service("foo").unwrap();
+        assert_eq!(service.name, "foo");
+        assert_eq!(service.alias, Some("bar".to_string()));
+
+        Ok(())
+    }
+
+    #[test]
+    fn given_valid_create_service_command_when_existing_service_name_then_should_fail() -> Result<(), Box<dyn Error>> {
+        let config = HtrsConfigBuilder::new()
+            .with_service(
+                ServiceBuilder::new()
+                    .with_name("existing_service_name")
+                    .with_alias("existing_service_alias")
+            )
+            .build();
+        setup(Some(config));
+
+        let mut cmd = Command::cargo_bin("htrs")?;
+        cmd.arg("new")
+            .arg("service")
+            .arg("existing_service_name");
+        cmd.assert()
+            .success();
+
+        Ok(())
+    }
 }
