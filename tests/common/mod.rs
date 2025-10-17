83873619f09a86821d73c6e1fd7f32dfbeb2eaac
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-17 21:41:05 +0100

    Moved helpers.rs to common/mod.rs

---
+++ tests/common/mod.rs
@@ -0,0 +1,99 @@
+use crate::common::config::{HtrsConfig, Service, VersionedHtrsConfig};
+use std::collections::HashMap;
+use std::fs::{remove_file, OpenOptions};
+use std::path::PathBuf;
+
+#[path = "../../src/config.rs"]
+mod config;
+
+pub fn get_config_path() -> PathBuf {
+    std::env::current_exe()
+        .expect("Unable to get executable location")
+        .parent()
+        .expect("Unable to get parent directory")
+        .parent()
+        .expect("Unable to get parent directory")
+        .join("config.json")
+}
+
+pub fn setup(init_config: Option<HtrsConfig>) {
+    let path = get_config_path();
+    if path.exists() {
+        remove_file(path.clone()).expect("Failed to clear existing config file");
+    }
+
+    if let Some(init_config) = init_config {
+        let handle = OpenOptions::new()
+            .create_new(true)
+            .write(true)
+            .open(path)
+            .unwrap();
+
+        let versioned_config = VersionedHtrsConfig::V0_0_1(init_config);
+        serde_json::to_writer_pretty(handle, &versioned_config).unwrap();
+    }
+}
+
+pub fn get_config() -> HtrsConfig {
+    let versioned_config: VersionedHtrsConfig = serde_json::from_reader(std::fs::File::open(get_config_path()).unwrap()).unwrap();
+    let VersionedHtrsConfig::V0_0_1(config) = versioned_config;
+    config
+}
+
+pub struct HtrsConfigBuilder {
+    pub services: Vec<Service>,
+}
+
+pub struct ServiceBuilder {
+    pub name: Option<String>,
+    pub alias: Option<String>,
+}
+
+impl HtrsConfigBuilder {
+    pub fn new() -> Self {
+        Self {
+            services: vec![],
+        }
+    }
+
+    pub fn with_service(mut self, builder: ServiceBuilder) -> Self {
+        self.services.push(builder.build());
+        self
+    }
+
+    pub fn build(self) -> HtrsConfig {
+        HtrsConfig {
+            services: self.services,
+            headers: HashMap::new(),
+        }
+    }
+}
+
+impl ServiceBuilder {
+    pub fn new() -> Self {
+        Self {
+            name: None,
+            alias: None,
+        }
+    }
+
+    pub fn with_name(mut self, name: &str) -> Self {
+        self.name = Some(name.to_string());
+        self
+    }
+
+    pub fn with_alias(mut self, alias: &str) -> Self {
+        self.alias = Some(alias.to_string());
+        self
+    }
+
+    pub fn build(self) -> Service {
+        Service {
+            name: self.name.unwrap(),
+            alias: self.alias,
+            headers: HashMap::new(),
+            endpoints: vec![],
+            environments: vec![],
+        }
+    }
+}
