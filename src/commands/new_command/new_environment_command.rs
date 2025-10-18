55ec0e8593850f72a4c843aaca93b2d87771bcdc
Mitchell Wright <36946520+Pseudooo@users.noreply.github.com>
2025-10-18 18:20:20 +0100

    New environment command (#97)

---
+++ src/commands/new_command/new_environment_command.rs
@@ -0,0 +1,90 @@
+use crate::command_builder::MatchBinding;
+use crate::config::{Environment, HtrsConfig};
+use crate::outcomes::HtrsAction::UpdateConfig;
+use crate::outcomes::{HtrsAction, HtrsError};
+use clap::{Arg, ArgMatches, Command};
+
+pub struct NewEnvironmentCommand {
+    pub service: String,
+    pub name: String,
+    pub alias: Option<String>,
+    pub host: String,
+    pub default: bool,
+}
+
+impl NewEnvironmentCommand {
+    pub fn get_command() -> Command {
+        Command::new("environment")
+            .visible_alias("env")
+            .about("Create a new environment")
+            .arg(
+                Arg::new("name")
+                    .help("The unique name for the new environment")
+                    .required(true)
+            )
+            .arg(
+                Arg::new("host")
+                    .help("The host for the environment")
+                    .required(true)
+            )
+            .arg(
+                Arg::new("default")
+                    .help("Flag to determine if the new environment should be the default")
+                    .required(false)
+                    .num_args(0)
+                    .long("default")
+            )
+            .arg(
+                Arg::new("alias")
+                    .help("The unique alias for the new environment")
+                    .required(false)
+                    .long("alias")
+                    .short('a')
+            )
+            .arg(
+                Arg::new("service")
+                    .help("The service that the environment will be created for")
+                    .required(true)
+                    .long("service")
+                    .short('s')
+            )
+    }
+
+    pub fn bind_from_matches(args: &ArgMatches) -> NewEnvironmentCommand {
+        NewEnvironmentCommand {
+            service: args.bind_field("service"),
+            name: args.bind_field("name"),
+            alias: args.bind_field("alias"),
+            host: args.bind_field("host"),
+            default: args.bind_field("default"),
+        }
+    }
+
+    pub fn execute(&self, config: &mut HtrsConfig) -> Result<HtrsAction, HtrsError> {
+        let Some(service) = config.get_service_mut(&self.service) else {
+            return Err(HtrsError::new(format!("No service found with name or alias `{}`", self.service).as_str()));
+        };
+
+        if service.get_environment(&self.name).is_some() {
+            return Err(HtrsError::new(format!("Service `{}` already has an environment with name or alias `{}`", service.name, self.name).as_str()));
+        }
+        if self.alias.is_some() && service.get_environment(self.alias.as_ref().unwrap().as_str()).is_some() {
+            return Err(HtrsError::new(format!("Service `{}` already has an environment with name or alias `{}`", service.name, self.alias.as_ref().unwrap()).as_str()));
+        }
+
+        if self.default {
+            if let Some(existing_default_environment) = service.get_default_environment_mut() {
+                existing_default_environment.default = false;
+            }
+        }
+
+        service.environments.push(
+            Environment::new(
+                self.name.clone(),
+                self.alias.clone(),
+                self.host.clone(),
+                self.default)
+        );
+        Ok(UpdateConfig)
+    }
+}
---
+++ tests/create_new_environment_tests.rs
@@ -0,0 +1,250 @@
+mod common;
+
+#[cfg(test)]
+mod create_new_environment_tests {
+    use crate::common::{get_config, setup, EnvironmentBuilder, HtrsConfigBuilder, ServiceBuilder};
+    use assert_cmd::Command;
+    use std::error::Error;
+
+    #[test]
+    fn given_create_environment_command_without_args_then_should_fail() -> Result<(), Box<dyn Error>> {
+        setup(None);
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .assert()
+            .failure();
+        Ok(())
+    }
+
+    #[test]
+    fn given_create_environment_command_without_service_arg_then_should_fail() -> Result<(), Box<dyn Error>> {
+        setup(None);
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .arg("foo_environment")
+            .arg("google.com")
+            .assert()
+            .failure();
+        Ok(())
+    }
+
+    #[test]
+    fn given_create_environment_command_with_unknown_service_then_should_fail() -> Result<(), Box<dyn Error>> {
+        setup(None);
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .arg("foo_environment")
+            .arg("google.com")
+            .arg("--service")
+            .arg("foo_service")
+            .assert()
+            .failure()
+            .stdout("No service found with name or alias `foo_service`\n");
+        Ok(())
+    }
+
+    #[test]
+    fn given_create_environment_command_with_known_service_then_should_succeed() -> Result<(), Box<dyn Error>> {
+        let config = HtrsConfigBuilder::new()
+            .with_service(
+                ServiceBuilder::new()
+                    .with_name("foo_service")
+            )
+            .build();
+        setup(Some(config));
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .arg("foo_environment")
+            .arg("google.com")
+            .arg("--service")
+            .arg("foo_service")
+            .assert()
+            .success();
+
+        let config = get_config();
+        let service = &config.services[0];
+        assert_eq!(service.environments.len(), 1);
+        let environment = &service.environments[0];
+        assert_eq!(environment.name, "foo_environment");
+        assert_eq!(environment.alias, None);
+        assert_eq!(environment.host, "google.com");
+        assert_eq!(environment.default, false);
+        Ok(())
+    }
+
+    #[test]
+    fn given_create_environment_command_with_alias_and_known_service_then_should_succeed() -> Result<(), Box<dyn Error>> {
+        let config = HtrsConfigBuilder::new()
+            .with_service(
+                ServiceBuilder::new()
+                    .with_name("foo_service")
+            )
+            .build();
+        setup(Some(config));
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .arg("foo_environment")
+            .arg("google.com")
+            .arg("--alias")
+            .arg("foo_alias")
+            .arg("--service")
+            .arg("foo_service")
+            .assert()
+            .success();
+
+        let config = get_config();
+        let service = &config.services[0];
+        assert_eq!(service.environments.len(), 1);
+        let environment = &service.environments[0];
+        assert_eq!(environment.name, "foo_environment");
+        assert_eq!(environment.alias, Some("foo_alias".to_string()));
+        assert_eq!(environment.host, "google.com");
+        assert_eq!(environment.default, false);
+        Ok(())
+    }
+
+    #[test]
+    fn given_create_environment_command_with_default_flag_and_known_service_then_should_succeed() -> Result<(), Box<dyn Error>> {
+        let config = HtrsConfigBuilder::new()
+            .with_service(
+                ServiceBuilder::new()
+                    .with_name("foo_service")
+            )
+            .build();
+        setup(Some(config));
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .arg("foo_environment")
+            .arg("google.com")
+            .arg("--service")
+            .arg("foo_service")
+            .arg("--default")
+            .assert()
+            .success();
+
+        let config = get_config();
+        let service = &config.services[0];
+        assert_eq!(service.environments.len(), 1);
+        let environment = &service.environments[0];
+        assert_eq!(environment.name, "foo_environment");
+        assert_eq!(environment.alias, None);
+        assert_eq!(environment.host, "google.com");
+        assert_eq!(environment.default, true);
+        Ok(())
+    }
+
+    #[test]
+    fn given_create_environment_command_with_existing_name_then_should_fail() -> Result<(), Box<dyn Error>> {
+        let config = HtrsConfigBuilder::new()
+            .with_service(
+                ServiceBuilder::new()
+                    .with_name("foo_service")
+                    .with_environment(
+                        EnvironmentBuilder::new()
+                            .with_name("foo_environment")
+                            .with_alias("foo_alias")
+                            .with_host("google.com")
+                    )
+            )
+            .build();
+        setup(Some(config));
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .arg("foo_environment")
+            .arg("google.com")
+            .arg("--alias")
+            .arg("new_alias")
+            .arg("--service")
+            .arg("foo_service")
+            .assert()
+            .failure()
+            .stdout("Service `foo_service` already has an environment with name or alias `foo_environment`\n");
+        Ok(())
+    }
+
+    #[test]
+    fn given_create_environment_command_with_existing_alias_then_should_fail() -> Result<(), Box<dyn Error>> {
+        let config = HtrsConfigBuilder::new()
+            .with_service(
+                ServiceBuilder::new()
+                    .with_name("foo_service")
+                    .with_environment(
+                        EnvironmentBuilder::new()
+                            .with_name("foo_environment")
+                            .with_alias("foo_alias")
+                            .with_host("google.com")
+                    )
+            )
+            .build();
+        setup(Some(config));
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .arg("new_environment")
+            .arg("google.com")
+            .arg("--alias")
+            .arg("foo_alias")
+            .arg("--service")
+            .arg("foo_service")
+            .assert()
+            .failure()
+            .stdout("Service `foo_service` already has an environment with name or alias `foo_alias`\n");
+        Ok(())
+    }
+
+    #[test]
+    fn given_create_environment_command_with_existing_default_then_should_succeed_and_replace_default() -> Result<(), Box<dyn Error>> {
+        let config = HtrsConfigBuilder::new()
+            .with_service(
+                ServiceBuilder::new()
+                    .with_name("foo_service")
+                    .with_environment(
+                        EnvironmentBuilder::new()
+                            .with_name("foo_environment")
+                            .with_alias("foo_alias")
+                            .with_host("google.com")
+                            .with_default()
+                    )
+            )
+            .build();
+        setup(Some(config));
+
+        Command::cargo_bin("htrs")?
+            .arg("new")
+            .arg("environment")
+            .arg("new_environment")
+            .arg("google.com")
+            .arg("--default")
+            .arg("--service")
+            .arg("foo_service")
+            .assert()
+            .success();
+
+        let config = get_config();
+        let service = &config.services[0];
+        assert_eq!(service.environments.len(), 2);
+        let existing_environment = &service.environments[0];
+        assert_eq!(existing_environment.default, false);
+        let new_environment = &service.environments[1];
+        assert_eq!(new_environment.name, "new_environment");
+        assert_eq!(new_environment.alias, None);
+        assert_eq!(new_environment.host, "google.com");
+        assert_eq!(new_environment.default, true);
+        Ok(())
+    }
+}
