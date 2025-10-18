e2ae206d706f363f52eab760b31bebdb0a935985
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-18 21:12:51 +0100

    Added delete environment command

--- src/commands/delete_command.rs
+++ src/commands/delete_command.rs
@@ -1,13 +1,16 @@
-use crate::commands::delete_command::DeleteCommand::Service;
+use crate::commands::delete_command::delete_environment_command::DeleteEnvironmentCommand;
+use crate::commands::delete_command::DeleteCommand::{Environment, Service};
 use crate::config::HtrsConfig;
 use crate::outcomes::{HtrsAction, HtrsError};
 use clap::{ArgMatches, Command};
 use delete_service_command::DeleteServiceCommand;
 
 mod delete_service_command;
+mod delete_environment_command;
 
 pub enum DeleteCommand {
     Service(DeleteServiceCommand),
+    Environment(DeleteEnvironmentCommand),
 }
 
 impl DeleteCommand {
@@ -17,11 +20,13 @@ impl DeleteCommand {
             .about("Delete an existing item from config")
             .arg_required_else_help(true)
             .subcommand(DeleteServiceCommand::get_command())
+            .subcommand(DeleteEnvironmentCommand::get_command())
     }
 
     pub fn bind_from_matches(args: &ArgMatches) -> DeleteCommand {
         match args.subcommand() {
             Some(("service", delete_service_matches)) => Service(DeleteServiceCommand::bind_from_matches(delete_service_matches)),
+            Some(("environment" | "env", delete_environment_matches)) => Environment(DeleteEnvironmentCommand::bind_from_matches(delete_environment_matches)),
             _ => unreachable!(),
         }
     }
@@ -29,6 +34,7 @@ impl DeleteCommand {
     pub fn execute(&self, config: &mut HtrsConfig) -> Result<HtrsAction, HtrsError> {
         match self {
             Service(delete_service_command) => delete_service_command.execute(config),
+            Environment(delete_environment_command) => delete_environment_command.execute(config),
         }
     }
 }
