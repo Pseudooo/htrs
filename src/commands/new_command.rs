ed65d683967aa93e876f645c1a6ada8ef3a7eba2
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-18 17:42:53 +0100

    Added new environment command

--- src/commands/new_command.rs
+++ src/commands/new_command.rs
@@ -1,7 +1,8 @@
 mod new_environment_command;
 mod new_service_command;
 
-use crate::commands::new_command::NewCommand::Service;
+use crate::commands::new_command::new_environment_command::NewEnvironmentCommand;
+use crate::commands::new_command::NewCommand::{Environment, Service};
 use crate::config::HtrsConfig;
 use crate::outcomes::{HtrsAction, HtrsError};
 use clap::{ArgMatches, Command};
@@ -9,6 +10,7 @@ use new_service_command::NewServiceCommand;
 
 pub enum NewCommand {
     Service(NewServiceCommand),
+    Environment(NewEnvironmentCommand),
 }
 
 impl NewCommand {
@@ -17,11 +19,13 @@ impl NewCommand {
             .about("Create a new item in config")
             .arg_required_else_help(true)
             .subcommand(NewServiceCommand::get_command())
+            .subcommand(NewEnvironmentCommand::get_command())
     }
 
     pub fn bind_from_matches(args: &ArgMatches) -> NewCommand {
         match args.subcommand() {
             Some(("service", service_matches)) => Service(NewServiceCommand::bind_from_matches(service_matches)),
+            Some(("environment" | "env", environment_matches)) => Environment(NewEnvironmentCommand::bind_from_matches(environment_matches)),
             _ => unreachable!(),
         }
     }
@@ -29,6 +33,7 @@ impl NewCommand {
     pub fn execute(&self, config: &mut HtrsConfig) -> Result<HtrsAction, HtrsError> {
         match self {
             Service(create_new_service_command) => create_new_service_command.execute(config),
+            Environment(create_new_environment_command) => create_new_environment_command.execute(config),
         }
     }
 }
