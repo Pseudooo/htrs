06241c76766d5cee1ac45988b912688e5fbf133c
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-18 23:12:43 +0100

    Added set header command

---
+++ src/commands/set_command.rs
@@ -0,0 +1,33 @@
+use crate::commands::set_command::set_header_command::SetHeaderCommand;
+use crate::commands::set_command::SetCommand::Header;
+use crate::config::HtrsConfig;
+use crate::outcomes::{HtrsAction, HtrsError};
+use clap::{ArgMatches, Command};
+
+mod set_header_command;
+
+pub enum SetCommand {
+    Header(SetHeaderCommand),
+}
+
+impl SetCommand {
+    pub fn get_command() -> Command {
+        Command::new("set")
+            .about("Set a value for an item in config")
+            .arg_required_else_help(true)
+            .subcommand(SetHeaderCommand::get_command())
+    }
+
+    pub fn bind_from_matches(args: &ArgMatches) -> SetCommand {
+        match args.subcommand() {
+            Some(("header", header_matches)) => Header(SetHeaderCommand::bind_from_matches(header_matches)),
+            _ => unreachable!(),
+        }
+    }
+
+    pub fn execute(&self, config: &mut HtrsConfig) -> Result<HtrsAction, HtrsError> {
+        match self {
+            Header(header) => header.execute(config),
+        }
+    }
+}
