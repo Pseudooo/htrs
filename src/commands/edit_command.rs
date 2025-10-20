4e5c0492c30b974bd439dc93dfba61e04ad588cc
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-20 22:46:48 +0100

    Added endpoint command

--- src/commands/edit_command.rs
+++ src/commands/edit_command.rs
@@ -1,5 +1,6 @@
+use crate::commands::edit_command::edit_endpoint_command::EditEndpointCommand;
 use crate::commands::edit_command::edit_environment_command::EditEnvironmentCommand;
-use crate::commands::edit_command::EditCommand::{Environment, Service};
+use crate::commands::edit_command::EditCommand::{Endpoint, Environment, Service};
 use crate::config::HtrsConfig;
 use crate::outcomes::{HtrsAction, HtrsError};
 use clap::{ArgMatches, Command};
@@ -7,10 +8,12 @@ use edit_service_command::EditServiceCommand;
 
 mod edit_service_command;
 mod edit_environment_command;
+mod edit_endpoint_command;
 
 pub enum EditCommand {
     Service(EditServiceCommand),
     Environment(EditEnvironmentCommand),
+    Endpoint(EditEndpointCommand),
 }
 
 impl EditCommand {
@@ -20,12 +23,14 @@ impl EditCommand {
             .arg_required_else_help(true)
             .subcommand(EditServiceCommand::get_command())
             .subcommand(EditEnvironmentCommand::get_command())
+            .subcommand(EditEndpointCommand::get_command())
     }
 
     pub fn bind_from_matches(args: &ArgMatches) -> EditCommand {
         match args.subcommand() {
             Some(("service", service_matches)) => Service(EditServiceCommand::bind_from_matches(service_matches)),
             Some(("environment" | "env", environment_matches)) => Environment(EditEnvironmentCommand::bind_from_matches(environment_matches)),
+            Some(("endpoint", endpoint_matches)) => Endpoint(EditEndpointCommand::bind_from_matches(endpoint_matches)),
             _ => unreachable!(),
         }
     }
@@ -34,6 +39,7 @@ impl EditCommand {
         match self {
             Service(edit_service_command) => edit_service_command.execute(config),
             Environment(edit_environment_command) => edit_environment_command.execute(config),
+            Endpoint(edit_endpoint_command) => edit_endpoint_command.execute(config),
         }
     }
 }
