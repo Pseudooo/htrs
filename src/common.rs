f328220974a96ae07c9ff3c1e8dc3f94f7dabd0a
Mitchell Wright <mitchell.wright877@gmail.com>
2025-10-26 23:14:59 +0000

    Resolved found issues

--- src/common.rs
+++ src/common.rs
@@ -1,5 +1,6 @@
 use lazy_static::lazy_static;
 use regex::Regex;
+use std::collections::HashSet;
 
 pub fn get_params_from_path(path: &str) -> Vec<String> {
     lazy_static! {
@@ -9,4 +10,17 @@ pub fn get_params_from_path(path: &str) -> Vec<String> {
         .filter_map(|s| s.as_str().parse().ok())
         .map(|s: String| s[1..s.len() - 1].to_string())
         .collect()
+}
+
+pub fn get_duplicates_from_vec(vec: Vec<String>) -> Vec<String> {
+    let mut duplicates: HashSet<String> = HashSet::new();
+    let mut set: HashSet<String> = HashSet::new();
+    for value in vec {
+        if !set.contains(&value) {
+            set.insert(value);
+        } else {
+            duplicates.insert(value);
+        }
+    }
+    duplicates.into_iter().collect()
 }
