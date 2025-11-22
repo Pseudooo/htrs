67d02476518e1092d97e886498d598afea154acc
Mitchell Wright <mitchell.wright877@gmail.com>
2025-11-22 15:32:40 +0000

    Corrected paths for script

--- .github/scripts/commentMutationComparison.js
+++ .github/scripts/commentMutationComparison.js
@@ -1,13 +1,13 @@
 module.exports = ({github, context, mutantsOutput}) => {
 
-    const featureTotalMutants = mutantsOutput.caught + mutantsOutput.missed;
-    const featureCaughtPercentage = mutantsOutput.caught / featureTotalMutants;
+    const featureTotalMutants = mutantsOutput.feature_caught + mutantsOutput.feature_missed;
+    const featureCaughtPercentage = mutantsOutput.feature_caught / featureTotalMutants;
 
     github.rest.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: `Caught ${featureCaughtPercentage.toFixed(2)}% of mutants! ${mutantsOutput.caught}/${featureTotalMutants}`,
+        body: `Caught ${featureCaughtPercentage.toFixed(2)}% of mutants! ${mutantsOutput.feature_caught}/${featureTotalMutants}`,
     });
 
 }
