7dadd771f01b450d5ac6f44f8ec6497e71ce4827
Mitchell Wright <36946520+Pseudooo@users.noreply.github.com>
2025-11-22 15:58:31 +0000

    Mutation testing workflow (#127)
    Introduced mutation testing workflow, use `/mutants` in pull request
    comments to run it and see comparison of mutations between PR & master

---
+++ .github/scripts/commentMutationComparison.js
@@ -0,0 +1,60 @@
+module.exports = ({github, context, mutantsOutput}) => {
+
+    const masterSummary = getMasterSummary(mutantsOutput);
+    const featureSummary = getFeatureSummary(mutantsOutput);
+
+    const percentageDiff = featureSummary.percentageCaught - masterSummary.percentageCaught;
+
+    let commentBody = "### Mutation Testing Completed\n";
+    if(percentageDiff > 0) {
+        commentBody += `:white_check_mark: Mutation Score Increased by +${percentageDiff.toFixed(2)}\n`;
+    } else if(percentageDiff < 0) {
+        commentBody += `:warning: Mutation score decreased by ${percentageDiff.toFixed(2)}\n`;
+    } else {
+        commentBody += `Mutation Score is unchanged\n`;
+    }
+    commentBody += '#### Summary'
+    commentBody += '| Target | Total Mutants | Caught | Missed | Percentage Caught |\n';
+    commentBody += '| ------ | ------------- | ------ | ------ | ----------------- |\n';
+    commentBody += getSummaryTableRow('Master', masterSummary);
+    commentBody += getSummaryTableRow('Branch', featureSummary);
+
+    github.rest.issues.createComment({
+        issue_number: context.issue.number,
+        owner: context.repo.owner,
+        repo: context.repo.repo,
+        body: commentBody,
+    });
+}
+
+function getMasterSummary(output) {
+    let caught = output.master_caught;
+    let missed = output.master_missed;
+    let total = caught + missed;
+    let percentageCaught = (caught / total) * 100
+
+    return {
+        caught: caught,
+        missed: missed,
+        total: total,
+        percentageCaught: percentageCaught.toFixed(2),
+    };
+}
+
+function getFeatureSummary(output) {
+    let caught = output.feature_caught;
+    let missed = output.feature_missed;
+    let total = caught + missed;
+    let percentageCaught = (caught / total) * 100
+
+    return {
+        caught: caught,
+        missed: missed,
+        total: total,
+        percentageCaught: percentageCaught.toFixed(2),
+    };
+}
+
+function getSummaryTableRow(branch, summary) {
+    return `| ${branch} | ${summary.total} | ${summary.caught} | ${summary.missed} | ${summary.percentageCaught.toFixed(2)}% |\n`;
+}
