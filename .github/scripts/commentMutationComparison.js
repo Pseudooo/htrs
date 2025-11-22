34494385465c021c26bbe3f54645ec19287c1f15
Mitchell Wright <mitchell.wright877@gmail.com>
2025-11-22 15:39:16 +0000

    Improved comment body

--- .github/scripts/commentMutationComparison.js
+++ .github/scripts/commentMutationComparison.js
@@ -1,13 +1,45 @@
 module.exports = ({github, context, mutantsOutput}) => {
 
-    const featureTotalMutants = mutantsOutput.feature_caught + mutantsOutput.feature_missed;
-    const featureCaughtPercentage = mutantsOutput.feature_caught / featureTotalMutants;
+    const masterSummary = getMasterSummary(mutantsOutput);
+    const featureSummary = getFeatureSummary(mutantsOutput);
+
+    let commentBody = "### Mutation Testing Completed\n"
+    commentBody += '| Target | Total Mutants | Caught | Missed | Percentage Caught |\n';
+    commentBody += '| ------ | ------------- | ------ | ------ | ----------------- |\n';
+    commentBody += `| Branch | ${featureSummary.total} | ${featureSummary.caught} | ${featureSummary.missed} | ${featureSummary.percentageCaught}% |\n`;
 
     github.rest.issues.createComment({
         issue_number: context.issue.number,
         owner: context.repo.owner,
         repo: context.repo.repo,
-        body: `Caught ${featureCaughtPercentage.toFixed(2)}% of mutants! ${mutantsOutput.feature_caught}/${featureTotalMutants}`,
+        body: commentBody,
     });
+}
 
+function getMasterSummary(output) {
+    let caught = output.master_caught;
+    let missed = output.master_missed;
+    let total = caught + missed;
+    let percentageCaught = (caught / total) * 100
+
+    return {
+        caught: caught,
+        missed: missed,
+        total: total,
+        percentageCaught: percentageCaught.toFixed(2),
+    };
+}
+
+function getFeatureSummary(output) {
+    let caught = output.feature_caught;
+    let missed = output.feature_missed;
+    let total = caught + missed;
+    let percentageCaught = (caught / total) * 100
+
+    return {
+        caught: caught,
+        missed: missed,
+        total: total,
+        percentageCaught: percentageCaught.toFixed(2),
+    };
 }
